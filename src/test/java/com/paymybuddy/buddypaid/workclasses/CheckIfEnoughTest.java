d01dd6d77af20ece20849d2cda57df30d923c033
Fercak Florian <fercak.florian@hotmail.fr>
2022-11-21 10:05:12 +0100

    ajout des tests pour la classe CheckIfEnough

---
+++ src/test/java/com/paymybuddy/buddypaid/workclasses/CheckIfEnoughTest.java
@@ -0,0 +1,34 @@
+package com.paymybuddy.buddypaid.workclasses;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CheckIfEnoughTest {
+	
+	CheckIfEnough checkIfEnough;
+	
+	@BeforeEach
+	public void init() {
+		checkIfEnough = new CheckIfEnough();
+	}
+	
+	@Test
+	public void testIsEnoughWhenCreditIsSufficient() {
+		/*ARRANGE*/
+		/*ACT*/
+		boolean result = checkIfEnough.isEnough(10, 0.05, 30, 10);
+		/*ASSERT*/
+		assertThat(result).isTrue();
+	}
+	
+	@Test
+	public void testIsEnoughWhenCreditIsNotSufficient() {
+		/*ARRANGE*/
+		/*ACT*/
+		boolean result = checkIfEnough.isEnough(10, 0.05, 20, 10);
+		/*ASSERT*/
+		assertThat(result).isFalse();
+	}
+}
