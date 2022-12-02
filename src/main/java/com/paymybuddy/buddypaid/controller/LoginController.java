179cc88bd25f6c2d7c843adb6adb120c53dc8911
Fercak Florian <fercak.florian@hotmail.fr>
2022-12-02 16:54:26 +0100

    gestion de l'affichage d'un message dans la page de login lors de la saisie d'identifiants errones

--- src/main/java/com/paymybuddy/buddypaid/controller/LoginController.java
+++ src/main/java/com/paymybuddy/buddypaid/controller/LoginController.java
@@ -2,13 +2,23 @@ package com.paymybuddy.buddypaid.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.paymybuddy.buddypaid.workclasses.FormComment;
 
 @Controller
 public class LoginController {
+	
+	private FormComment errorLoginFormComment = new FormComment();
 
 	@GetMapping("/login")
-	public String logIn() {
+	public String login(Model model, @RequestParam(name = "error", defaultValue = "false") boolean error) {
+		System.out.println("La valeur de error est : " + error);
+		errorLoginFormComment.setError(error);
+		errorLoginFormComment.setMessage("bad credentials");
+		model.addAttribute("formComment", errorLoginFormComment);
 		return "login";
 	}
 }
