e3c3f827c73dad1e45f0e7de07df87fb41819ec3
Fercak Florian <fercak.florian@hotmail.fr>
2022-12-02 16:56:51 +0100

    nettoyage de methode login

--- src/main/java/com/paymybuddy/buddypaid/controller/LoginController.java
+++ src/main/java/com/paymybuddy/buddypaid/controller/LoginController.java
@@ -15,7 +15,6 @@ public class LoginController {
 
 	@GetMapping("/login")
 	public String login(Model model, @RequestParam(name = "error", defaultValue = "false") boolean error) {
-		System.out.println("La valeur de error est : " + error);
 		errorLoginFormComment.setError(error);
 		errorLoginFormComment.setMessage("bad credentials");
 		model.addAttribute("formComment", errorLoginFormComment);
