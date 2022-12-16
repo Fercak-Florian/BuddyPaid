ddc3b6580338fd05e9b172033a0e1677ec6d08f1
Fercak Florian <96480018+Fercak-Florian@users.noreply.github.com>
2022-12-16 19:01:04 +0100

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,36 @@
+/*Initialisation*/
+
+DROP DATABASE paymybuddy;
+CREATE DATABASE paymybuddy;
+USE paymybuddy;
+
+/*Creation des différentes tables*/
+CREATE TABLE user
+(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	login VARCHAR (30) NOT NULL,
+    password VARCHAR (250) NOT NULL,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE operation
+(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    buddy_id INTEGER NOT NULL,
+    FOREIGN KEY (buddy_id) REFERENCES user (id),
+    date DATE,
+    amount DOUBLE NOT NULL,
+    description VARCHAR (250)
+);
+
+CREATE TABLE user_buddy
+(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    buddy_id INTEGER NOT NULL,
+    FOREIGN KEY (buddy_id) REFERENCES user (id)
+);
